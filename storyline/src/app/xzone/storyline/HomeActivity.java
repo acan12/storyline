63d5e264f4e04d704db676a44268f3c1727ed708
arycancer <arycancer@gmail.com>
2013-05-29 05:59:53 +0700

    testing commit

--- storyline/src/app/xzone/storyline/HomeActivity.java
+++ storyline/src/app/xzone/storyline/HomeActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class HomeActivity extends Activity {
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. Testing */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
