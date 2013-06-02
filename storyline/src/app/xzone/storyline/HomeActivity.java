fe42dc9506990c2ba42fa6063d2e13b3d16ecd58
arycancer <arycancer@gmail.com>
2013-06-02 10:53:43 +0700

    add form layout to input your new story

--- storyline/src/app/xzone/storyline/HomeActivity.java
+++ storyline/src/app/xzone/storyline/HomeActivity.java
@@ -1,13 +1,34 @@
 package app.xzone.storyline;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class HomeActivity extends Activity {
-    /** Called when the activity is first created. Testing */
+public class HomeActivity extends Activity implements OnClickListener{
+    private ImageButton _addButton;
+	private Intent intent;
+
+	/** Called when the activity is first created. Testing */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        _addButton = (ImageButton) findViewById(R.id.imgAddButton);
+        
+        _addButton.setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		if(v == _addButton) {
+			intent = new Intent(this, FormActivity.class);
+		}
+		
+		startActivity(intent);
+	}
 }
