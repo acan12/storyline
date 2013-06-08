cfdeccfc91966d5021426cf93cc05bf3d79f5a00
arycancer <arycancer@gmail.com>
2013-06-08 17:13:25 +0700

    add sample sliding menu in android + put message and icons into buble image

--- storyline/src/app/xzone/storyline/FormActivity.java
+++ storyline/src/app/xzone/storyline/FormActivity.java
@@ -1,14 +1,221 @@
 package app.xzone.storyline;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class FormActivity extends Activity {
+
+import java.util.Calendar;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.TimePicker.OnTimeChangedListener;
+
+public class FormActivity extends Activity implements OnClickListener{
+
+	private Button startDateButton;
+	private Button startTimeButton;
+	private Button endDateButton;
+	private Button endTimeButton;
+	private DateTime dt = new DateTime();
+	
+	static final int DATE_DIALOG_ID = 0;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.form);
 		
+		Log.d("info", "testing entrance form activity");
+		
+		startDateButton = (Button) findViewById(R.id.startDateButton);
+		startTimeButton = (Button) findViewById(R.id.startTimeButton);
+		endDateButton 	= (Button) findViewById(R.id.endDateButton);
+		endTimeButton 	= (Button) findViewById(R.id.endTimeButton);
+		
+		startDateButton.setOnClickListener(this);
+		startTimeButton.setOnClickListener(this);
+		endDateButton.setOnClickListener(this);
+		endTimeButton.setOnClickListener(this);
+		
 	}
+
+	public void showCustomDateTimeDialog(){
+		DateTime dt = new DateTime();
+		
+//		
+		
+//		DatePickerDialog dp = new DatePickerDialog(this, new OnDateSetListener() {
+//			
+//			@Override
+//			public void onDateSet(DatePicker view, int year, int month,
+//					int day) {
+//				// TODO Auto-generated method stub
+//				
+//				DateTimeFormatter fmt = DateTimeFormat.forPattern("MMM dd,yyyy");
+//				DateTime dt2 = new DateTime(year, month, day, 0, 0, 0, 0);
+//				String datetime = dt2.toString(fmt);
+//				
+//				EditText startDateText = (EditText) findViewById(R.id.startDateText);
+//				startDateText.setText(datetime);
+//				
+//			}
+//		}, dt.getYear(), dt.getMonthOfYear(), dt.getDayOfMonth());
+//		dp.show();
+	}
+	
+	
+	
+	@Override
+	public void onClick(View v) {
+		DatePickerDialog dp;
+		TimePickerDialog tp;
+		// TODO Auto-generated method stub
+		switch (v.getId()){
+		case R.id.startDateButton:
+			Log.d("info", "testing startDateButton");
+			
+			dp = new DatePickerDialog(this, new OnDateSetListener() {
+				
+				@Override
+				public void onDateSet(DatePicker view, int year, int month,
+						int day) {
+					// TODO Auto-generated method stub
+					
+					DateTimeFormatter fmt = DateTimeFormat.forPattern("MMM dd,yyyy");
+					DateTime dt2 = new DateTime(year, month, day, 0, 0, 0, 0);
+					String datetime = dt2.toString(fmt);
+					
+					EditText startDateText = (EditText) findViewById(R.id.startDateText);
+					startDateText.setText(datetime);
+					
+				}
+			}, dt.getYear(), dt.getMonthOfYear(), dt.getDayOfMonth());
+			dp.show();
+			
+			break;
+		case R.id.startTimeButton:
+			Log.d("info", "testing startTimeButton");
+			
+			tp = new TimePickerDialog(this, new OnTimeSetListener() {
+				
+				@Override
+				public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+					// TODO Auto-generated method stub
+					EditText startTimeText = (EditText) findViewById(R.id.startTimeText);
+					startTimeText.setText(hourOfDay+":"+minute);
+				}
+			}, dt.getHourOfDay(), dt.getMinuteOfHour(), true);
+			tp.show();
+			
+			break;
+		case R.id.endDateButton:
+			Log.d("info", "testing endDateButton");
+			
+			dp = new DatePickerDialog(this, new OnDateSetListener() {
+				
+				@Override
+				public void onDateSet(DatePicker view, int year, int month,
+						int day) {
+					// TODO Auto-generated method stub
+					
+					DateTimeFormatter fmt = DateTimeFormat.forPattern("MMM dd,yyyy");
+					DateTime dt2 = new DateTime(year, month, day, 0, 0, 0, 0);
+					String datetime = dt2.toString(fmt);
+					
+					EditText endDateText = (EditText) findViewById(R.id.endDateText);
+					endDateText.setText(datetime);
+					
+				}
+			}, dt.getYear(), dt.getMonthOfYear(), dt.getDayOfMonth());
+			dp.show();
+			
+			break;
+		case R.id.endTimeButton:
+			tp = new TimePickerDialog(this, new OnTimeSetListener() {
+				
+				@Override
+				public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+					// TODO Auto-generated method stub
+					EditText endTimeText = (EditText) findViewById(R.id.endTimeText);
+					endTimeText.setText(hourOfDay+":"+minute);
+				}
+			}, dt.getHourOfDay(), dt.getMinuteOfHour(), true);
+			tp.show();
+			
+			
+			break;
+//		case R.id.bMapPicker:
+//			Intent mapIntent = new Intent(this, MapLocationActivity.class); 
+//			mapIntent.putExtra(PARAMS_KEY, paramid);
+//			startActivityForResult(mapIntent, paramid);
+//
+//			break; 
+//		case R.id.btn_submit:
+//			a =new ActivityEvent();
+//			DBAdapter db = new DBAdapter(this);
+//			
+//			
+//			if(action > 0){
+//				TextView t = (TextView)findViewById(R.id.hidden_value);
+//				a.setId(Integer.parseInt(t.getText().toString()));
+//				setActivityValue(a);
+//				db.updateRecord(a);
+//				
+//				Intent i = new Intent(this, DetailActivity.class);
+//		    	i.putExtra(PARAMS_KEY, a.getId());
+//				startActivity(i); 
+//				
+//			} else {
+//				setActivityValue(a);
+//				db.insertRecord(a);
+//				finish();
+//			}
+//			
+//			break;
+//		case R.id.icon_activity:
+//			String[] labels = idIcon.getLabels();
+//			final int[] icons = idIcon.getIcons();
+//			AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+//			dialog.setTitle("Choose your Activity icon");
+//			dialog.setAdapter(getDialogAdapter(labels, icons), new DialogInterface.OnClickListener() {
+//
+//				@Override
+//				public void onClick(DialogInterface dialog, int which) {
+//					// TODO Auto-generated method stub
+//					ImageView img =(ImageView)findViewById(R.id.icon_activity);
+//            		Drawable drawable = getResources().getDrawable(icons[which]);
+//            		img.setImageDrawable(drawable);
+//            		
+//            		//set label icon name
+//            		TextView eIcon = (TextView)findViewById(R.id.icon_label);
+//            		eIcon.setText(idIcon.getLabels()[which]);
+//					
+//				}});
+//			dialog.show();
+//		}
+//		
+//		if (v == homeButton) {
+//			Intent i = new Intent();
+//			i.setClass(this, HomeActivity.class);
+//			i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//			startActivity(i);
+		}
+	}
+
 }
--- storyline/src/app/xzone/storyline/HomeActivity.java
+++ storyline/src/app/xzone/storyline/HomeActivity.java
@@ -1,34 +1,170 @@
 package app.xzone.storyline;
 
+import java.util.Date;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+import app.xzone.storyline.libs.CustomHorizontalScrollView;
+import app.xzone.storyline.libs.CustomHorizontalScrollView.SizeCallback;
+import app.xzone.storyline.libs.ViewUtils;
 
-public class HomeActivity extends Activity implements OnClickListener{
-    private ImageButton _addButton;
+public class HomeActivity extends Activity implements OnClickListener {
+	private ImageButton _addButton;
 	private Intent intent;
 
+	private CustomHorizontalScrollView scrollView;
+	private View menu;
+	private View app;
+	private ImageView btnSlide;
+	boolean menuOut = false;
+	Handler handler = new Handler();
+	int btnWidth;
+	private ImageButton _listButton;
+
 	/** Called when the activity is first created. Testing */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        _addButton = (ImageButton) findViewById(R.id.imgAddButton);
-        
-        _addButton.setOnClickListener(this);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		 setContentView(R.layout.main);
+		
+		 Log.d("info", "testing entrance form activity");
+		
+		 _addButton = (ImageButton) findViewById(R.id.imgAddButton);
+		 _listButton = (ImageButton) findViewById(R.id.menuListButton);
+		
+		 _addButton.setOnClickListener(this);
+		 _listButton.setOnClickListener(this);
+
+		// implement sliding menu
+
+		// LayoutInflater inflater = LayoutInflater.from(this);
+		// scrollView = (CustomHorizontalScrollView)
+		// inflater.inflate(R.layout.horz_scroll_with_list_menu, null);
+		// setContentView(scrollView);
+		//
+		// menu = inflater.inflate(R.layout.horz_scroll_menu, null);
+		// app = inflater.inflate(R.layout.horz_scroll_app, null);
+		// ViewGroup tabBar = (ViewGroup) findViewById(R.id.tabBar);
+		//
+		// ListView listView = (ListView) app.findViewById(R.id.list);
+		// ViewUtils.initListView(this, listView, "Item ", 30,
+		// android.R.layout.simple_list_item_1);
+		//
+		// listView = (ListView) menu.findViewById(R.id.list);
+		// ViewUtils.initListView(this, listView, "Menu", 30,
+		// android.R.layout.simple_list_item_1);
+		//
+		// btnSlide = (ImageView) tabBar.findViewById(R.id.BtnSlide);
+		// btnSlide.setOnClickListener(new ClickListenerForScrolling(scrollView,
+		// menu));
+		//
+		// final View[] children = new View[] { menu, app };
+		//
+		// // Scroll to app (view[1]) when layout finished.
+		// int scrollToViewIdx = 1;
+		// scrollView.initViews(children, scrollToViewIdx,
+		// new SizeCallbackForMenu(btnSlide));
+
+
+	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		if(v == _addButton) {
+		if (v == _addButton) {
 			intent = new Intent(this, FormActivity.class);
+		}else if(v == _listButton){
+			intent = new Intent(this, HorzScrollWithListMenu.class);
 		}
-		
+
 		startActivity(intent);
+	}
+
+	// core
+	/**
+	 * Helper for examples with a HSV that should be scrolled by a menu View's
+	 * width.
+	 */
+	static class ClickListenerForScrolling implements OnClickListener {
+		HorizontalScrollView scrollView;
+		View menu;
+		/**
+		 * Menu must NOT be out/shown to start with.
+		 */
+		boolean menuOut = false;
+
+		public ClickListenerForScrolling(HorizontalScrollView scrollView,
+				View menu) {
+			super();
+			this.scrollView = scrollView;
+			this.menu = menu;
+		}
+
+		@Override
+		public void onClick(View v) {
+			Context context = menu.getContext();
+			String msg = "Slide " + new Date();
+			Toast.makeText(context, msg, 1000).show();
+			System.out.println(msg);
+
+			int menuWidth = menu.getMeasuredWidth();
+
+			// Ensure menu is visible
+			menu.setVisibility(View.VISIBLE);
+
+			if (!menuOut) {
+				// Scroll to 0 to reveal menu
+				int left = 0;
+				scrollView.smoothScrollTo(left, 0);
+			} else {
+				// Scroll to menuWidth so menu isn't on screen.
+				int left = menuWidth;
+				scrollView.smoothScrollTo(left, 0);
+			}
+			menuOut = !menuOut;
+		}
+	}
+
+	/**
+	 * Helper that remembers the width of the 'slide' button, so that the
+	 * 'slide' button remains in view, even when the menu is showing.
+	 */
+	static class SizeCallbackForMenu implements SizeCallback {
+		int btnWidth;
+		View btnSlide;
+
+		public SizeCallbackForMenu(View btnSlide) {
+			super();
+			this.btnSlide = btnSlide;
+		}
+
+		@Override
+		public void onGlobalLayout() {
+			btnWidth = btnSlide.getMeasuredWidth();
+			System.out.println("btnWidth=" + btnWidth);
+		}
+
+		@Override
+		public void getViewSize(int idx, int w, int h, int[] dims) {
+			dims[0] = w;
+			dims[1] = h;
+			final int menuIdx = 0;
+			if (idx == menuIdx) {
+				dims[0] = w - btnWidth;
+			}
+		}
 	}
 }
